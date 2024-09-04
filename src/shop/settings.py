374629e402dff66ac78673c890f0c42ceb9e2678
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-09-04 12:57:59 -0500

    Secret Key

--- src/shop/settings.py
+++ src/shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q*y!c$dh4b1wk9t54nc-6(1r$agy!4e4482k(pb*d37)4s)y39'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
