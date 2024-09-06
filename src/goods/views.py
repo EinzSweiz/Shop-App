4fe3df3b871597c6f657ba6b140532e04787a3c7
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-09-06 06:18:31 -0500

    Second pagination options

--- src/goods/views.py
+++ src/goods/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from goods.models import Products, Categories
+from .models import Products, Categories
 from django.core.paginator import Paginator
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
     if category_slug == 'all-products':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products, category__slug=category_slug)
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     context = {
         'title': 'Home-Catalog',
         'goods': current_page,
