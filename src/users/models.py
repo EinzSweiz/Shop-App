96d7892689b8cab6924657370bbc703146c5e993
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-09-07 08:05:45 -0500

    Login Registration Logout wievs permission forms

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+
+    def __str__(self) -> str:
+        return self.username
+    
+    groups = models.ManyToManyField(
+    'auth.Group',
+    related_name='custom_user_set',  # Avoid clash with 'auth.User'
+    blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set_permissions',  # Avoid clash with 'auth.User'
+        blank=True
+    )
--- src/users/views.py
+++ src/users/views.py
@@ -1,23 +1,57 @@
-from django.shortcuts import render
-from django.contrib.auth import logout
 
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from .forms import UserLoginForm, UserRegistrationForm
+from django.contrib import messages
 
 def login_user(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)  # Pass request and data for authentication
+        if form.is_valid():
+            username = form.cleaned_data['username']  # Access cleaned data
+            password = form.cleaned_data['password']  # Access cleaned data
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, 'You are logged in')
+                return redirect('main:home')
+            else:
+                # Add a non-field error if authentication fails
+                form.add_error(None, 'Invalid username or password')
+        else:
+            # Add a non-field error if form submission is invalid
+            form.add_error(None, 'Invalid form submission')
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Login User'
+        'title': 'Login User',
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
-
 def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('main:home')
+    else:
+        form = UserRegistrationForm()
     context = {
-        'title': 'Login User'
+        'title': 'Login User',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
 def logout_user(request):
     logout(request)
+    return redirect('main:home')
     
 
 def profile_user(request):
